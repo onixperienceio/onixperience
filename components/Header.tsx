ae8e249bf99cda7ef20e20756e3ba8f8632cdb2b
sofffja <sofi.efron@gmail.com>
2023-06-06 18:26:11 -0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
             </a>
           </Link>
         </div>
-        <p className="font-display sm:text-5xl text-3xl sm:pt-1 pt-3"><Link href="/"
+        <p className="font-display sm:text-4xl text-3xl sm:pt-2 pt-3"><Link href="/"
           >
             <a className={router.pathname == "/" ? "active" : ""}>
               {/* <img className="max-h-[5em]" src="/logo1.png" alt="" /> */}
