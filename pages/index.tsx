e027067916f8d07f008c6c53635443e6f677e5fb
onixperienceio <133596092+onixperienceio@users.noreply.github.com>
2024-05-13 18:28:41 +0200

    Fixed frameborder and async script issues in index.tsx as per Next.js best practices.

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,9 @@ const Home: NextPage = () => {
             </Head>
             <main className="flex justify-center items-center">
                 <div style={{padding: '56.25% 0 0 0', position: 'relative'}}>
-                    <iframe src="https://player.vimeo.com/video/944843505?h=676b3d6289&badge=0&autopause=0&player_id=0&app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} title="Onix Show Reel"></iframe>
+                    <iframe src="https://player.vimeo.com/video/944843505?h=676b3d6289&badge=0&autopause=0&player_id=0&app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} title="Onix Show Reel"></iframe>
                 </div>
-                <script src="https://player.vimeo.com/api/player.js"></script>
+                <script src="https://player.vimeo.com/api/player.js" async></script>
             </main>
         </div>
     )
