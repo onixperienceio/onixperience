e47db732da4fe131c398ba5ef7b74953153e4324
onixperienceio <133596092+onixperienceio@users.noreply.github.com>
2024-05-17 01:13:11 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ const Home: NextPage = () => {
             <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="flex justify-center items-center">
-                <div style={{padding: '246px 0px', position: 'relative', height: '100%' , width: '100%'}}>
-                    <iframe src="https://player.vimeo.com/video/944843505?h=676b3d6289&badge=0&autopause=0&player_id=0&app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{borderStyle: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '1120px'}} title="Onix Show Reel"></iframe>
+                <div style={{padding: '20.15% 0px', position: 'relative', height: '100%' , width: '100%'}}>
+                    <iframe src="https://player.vimeo.com/video/944843505?h=676b3d6289&badge=0&autopause=0&player_id=0&app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{borderStyle: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '1075px'}} title="Onix Show Reel"></iframe>
                 </div>
                 <script src="https://player.vimeo.com/api/player.js" async></script>
             </main>
