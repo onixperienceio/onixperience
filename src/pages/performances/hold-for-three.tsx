d0cf72428bdcc819e1a4b717ef82dc581715d52a
onixperienceio <133596092+onixperienceio@users.noreply.github.com>
2024-05-23 22:07:32 +0200

    Add hold-for-three.tsx file

---
+++ src/pages/performances/hold-for-three.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+
+const HoldForThree = () => (
+  <div>
+    <h1>HOLD FOR THREE</h1>
+    <h2>Sonic Performance</h2>
+    <h3>2023</h3>
+    <p>
+      ‘Hold for Three’ is a sonic performance which morphs bodies into machinehood and objects into organs. Kayla Elrod and Lottie Sebes probe the sonic capacities of various bellows, operating them as instruments, extensions and prosthetics.
+    </p>
+    <p>
+      Sonic worlds are built from wheezing breath, creaking hinges, loosening screws, and hot air hissing.
+    </p>
+    <p>
+      Outmoded objects of production are transformed into otherworldly organs, powered by mechanical respiration, a cycle of tension and release. What slips from a hosed mouth on the floor of an old ship?
+    </p>
+    <p>Funded by Neue Initiative Musik</p>
+    <p>2023 Hošek Contemporary/ Berlin, Germany</p>
+    <Image src="/images/performance/hold-for-three/portada.jpg" alt="Portada" width={800} height={600} />
+    <Image src="/images/performance/hold-for-three/1.jpg" alt="1" width={800} height={600} />
+    <Image src="/images/performance/hold-for-three/2.jpg" alt="2" width={800} height={600} />
+    <Image src="/images/performance/hold-for-three/3.jpg" alt="3" width={800} height={600} />
+    <Image src="/images/performance/hold-for-three/4.jpg" alt="4" width={800} height={600} />
+    <h3>CREDITS</h3>
+    <p>
+      INITIAL CONCEPT & PERFORMANCE, OBJECT DESIGN ASSISTANCE BY Lottie Sebes<br />
+      CONCEPT DEVELOPMENT & PERFORMANCE, OBJECT DESIGN ASSISTANCE BY Kayla Celrod<br />
+      ASSISTANCE BY Kayla Celrod<br />
+      OBJECT & SET DESIGN BY Anna Maddalena Cingi<br />
+      SOUND DESIGN BY Aleksander Filipiak<br />
+      DRAMATURGIC COLLABORATION & ACTING COACH BY ONIX-Victoria Momeño<br />
+      GRAPHIC DESIGN BY David Luciani<br />
+      DOCUMENTATION BY Kim Wichera<br />
+      THANKS TO Field Notes Berlin Magazin
+    </p>
+  </div>
+);
+
+export default HoldForThree;
