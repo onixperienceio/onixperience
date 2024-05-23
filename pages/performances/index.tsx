518d997354f6bfa5ef2943721a05a0b29685c20f
onixperienceio <133596092+onixperienceio@users.noreply.github.com>
2024-05-23 22:24:06 +0200

    Update index.tsx

--- pages/performances/index.tsx
+++ pages/performances/index.tsx
@@ -21,29 +21,29 @@ const Performances: NextPage = () => {
     const performances = [
         {
             href: "performances/trance4mation",
-            title: 'Chaos Uranus',
-            category: 'Drag performance',
+            title: 'TRANCE4MATION',
+            category: 'performance',
             year: '2022-23',
             image: 'trance4mation/portada.jpg'
         },
         {
             href: "performances/hybrid-pleasures",
-            title: 'CC_LAB',
-            category: 'Sonic Performance',
+            title: 'HYBRID PLEASURES',
+            category: 'performance',
             year: '2022',
             image: 'hybrid-pleasures/portada.jpg'
         },
         {
             href: "performances/reddish-foam",
             title: 'REDDISH FOAM',
-            category: 'Performance',
+            category: 'performance',
             year: '2021',
             image: 'reddish-foam/portada.jpg'
         },
         {
             href: "performances/fuck-me",
             title: 'FUCK ME',
-            category: 'Performance',
+            category: 'performance',
             year: '2020',
             image: 'fuckme/portada.jpg'
         },
@@ -57,14 +57,14 @@ const Performances: NextPage = () => {
         {
             href: "performances/optical-instrument",
             title: 'OPTICAL INSTRUMENT',
-            category: 'Sonic Performance',
+            category: 'performance',
             year: '2019',
             image: 'io/portada.jpg'
         },
         {
             href: "performances/hold-for-three",
             title: 'HOLD FOR THREE',
-            category: 'Sonic Performance',
+            category: 'performance',
             year: '2023',
             image: 'hold-for-three/portada.jpg'
         }
