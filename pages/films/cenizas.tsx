a8261dacfe2b0c1cee97a491300c8819ebf67f31
onixperienceio <133596092+onixperienceio@users.noreply.github.com>
2024-05-24 14:10:10 +0200

    Update cenizas.tsx

--- pages/films/cenizas.tsx
+++ pages/films/cenizas.tsx
@@ -5,28 +5,28 @@ import { ImageGallery } from '../../components/ImageGallery'
 import { Credits } from '../../components/Credits'
 
 const Film: NextPage = () => {
-    const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
-    const carpeta = 'indeseables'
+    const images = ['1.jpg', '2.jpg'];
+    const carpeta = 'cenizas'
     const category = 'film'
 
     const creditos = [
-        ['EXECUTIVE PRODUCER & CHOREOGRAPHY BY', 'ONIX-Victoria Momeño'],
-        ['DIRECTED BY', 'Ángela Amarilla & Maximiliano Trionfante'],
-        ['PERFORMED BY', 'Guadalupe Sanz, Guido Veneroni, Horacio Pieroni'],
+        ['WRITTEN & DIRECTED BY', 'Gabriela Orlandi'],
+        ['PRODUCTION MANAGEMENT BY', 'Matias Orlandi'],
+        ['POSTPRODUCTION MANAGEMENT BY', 'Maximiliano Trionfante & Victoria Momeño'],
         ['EDITED BY', 'Maximiliano Trionfante'],
-        ['SCRIPT BY', 'Ángela Amarilla & Maximiliano Trionfante'],
-        ['BASED IN', 'The Play “Alien Love” by Ángela Amarilla'],
+        ['DIRECTOR ASSISTANCE BY', 'Gary Koretzky'],
+        ['PHOTOGRAPHY DIRECTION BY', 'Emanuel Sarmiento'],
         ['ART DIRECTION BY', 'Victoria Carel'],
-        ['PHOTOGRAPHY DIRECTION BY', 'Pablo Lozano'],
+        ['GAFFER', 'Evelyn Denise Pin'],
         ['ASSISTANCE BY', 'Paula Menga'],
         ['COREOGRAPHY COLLABORATION BY', 'Carla Di Grazia'],
         ['COSTUMES BY', 'Catalina di Primio'],
-        ['FX MAKE UP BY', 'Victor Krueger'],
-        ['COLOUR BY', 'Bruno Noaro'],
+        ['MAKE UP BY', 'Cecilia Cristaldo'],
+        ['COLOUR BY', 'Patricia Batlle'],
         ['VFX BY', 'Guido Ferraro'],
-        ['SOUND BY', 'Gastón Ibarroule & Facundo Paco Giron'],
-        ['TRANSLATION BY', 'Ezequiel Zaidenwerg'],
-        ['THANKS TO', 'Alejandro Casagrande']
+        ['SOUND BY', 'ignacio Blanco & Gastón Ibarroule'],
+        ['THANKS TO', 'Alejandro Casagrande'],
+        ['PRODUCED BY', 'NEON REBELS']
     ]
 
     return (
@@ -38,14 +38,14 @@ const Film: NextPage = () => {
             </Head>
             <main className="">
                 <ProjectPage
-                    title='Undesirables'
+                    title='Cenizas'
                     subtitle='short film'
-                    year={`2021`}
+                    year={`2024`}
                     opening=''
                     category={category}
                     imageCover={`/${carpeta}/portada.jpg`}
                     description={<>
-                        A man has been tied up for days. Today he&apos;s not alone, it&apos;s Wednesday. His wounds, dimly visible in the faint light, serve as a painful reminder that he is still alive. How deep can desires drill? Seeing the woman of your dreams swing to a 60s beat, knife in hand—perhaps this is what paradise looks like.
+                        (soon)
                     </>}
                     credits={<>
                         {creditos.map((credit) => (
