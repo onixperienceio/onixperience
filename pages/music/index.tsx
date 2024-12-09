a74c0460537c779cbafdd4ee022a5d8a483b4f8a
onixperienceio <133596092+onixperienceio@users.noreply.github.com>
2024-12-09 04:19:38 +0100

    Update index.tsx

--- pages/music/index.tsx
+++ pages/music/index.tsx
@@ -1,67 +1,56 @@
-import type { NextPage } from 'next'
-import Link from 'next/link'
-import Head from 'next/head'
-import Image from 'next/image'
-import { ProjectsHeader } from '../../components/ProjectsHeader'
-import { ProjectPage } from '../../components/ProjectPage'
-import { ImageGallery } from '../../components/ImageGallery'
-import { Credits } from '../../components/Credits'
-
-// este es un ejemplo de pagina interna de proyecto particular
-// no esta en el menu pero si vas a onixperience.io/music se puede ver
-// una vez que quieras que aparezca en el menu vas a components/ProjectsHeader.tsx y ahi agregas el link en el menu
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import Head from 'next/head';
+import { ProjectPage } from '../../components/ProjectPage';
+import { Credits } from '../../components/Credits';
 
 const Music: NextPage = () => {
-    const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
-    const carpeta = 'music' // aca va el nombre de la carpeta donde subiste las fotos de esta carpeta
+    const carpeta = 'music'; // Folder for images (currently unused)
 
-    // si no hay creditos, podes borrar todo y dejar solo los corchetes asi:
-    // const creditos = []
     const creditos = [
         ['PRODUCED BY', 'Camilo Desorden'],
         ['SONG-WRITER & VOCALS BY', 'ONIX'],
         ['VIDEO BY', 'Max Trionfante'],
-    ]
+    ];
 
     return (
-        <div className='projects-container'>
+        <div className="projects-container">
             <Head>
-            <title>ONIX</title>
-            <meta name="description" content="ONIX" />
-            <link rel="icon" href="/favicon.ico" />
+                <title>ONIX - Music</title>
+                <meta name="description" content="ONIX Music Section" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="">
                 <ProjectPage
-                // aca lo que quieras que quede vacio lo dejas solo con comillas ''
-                // los creditos y las imagenes solo se van a mostrar si el las listas de arriba hay algo, en esta parte dejalo tal cual 
-                    title='Music'
-                    subtitle = ''
-                    year=''
-                    opening=''
-                    category=''
+                    title="Music"
+                    subtitle=""
+                    year=""
+                    opening=""
+                    category=""
                     imageCover={`${carpeta}/portada.jpg`}
-                    description={<>
-                       Singer-songwriter presenting a work-in-progress album, Neocity.</span> <br />
-                       An album that delves into the many sides of Berlin, a mix of hyperpop, with latin rhythms like cumbia and trap, . <br />
-                       a celebration of self and yet a journey into the visceral. These openings serve as an exploration of the deeper meanings within the songs<br />
-                       an exploration through the ritual , the sacred and the revelry , the impulsive, embracing the latin cosmovision of finding joy despite everything. <br /> 
-                        </span>
-                    </>}
-                    credits={<>
-                        {creditos.map((credit) => (
-                        <Credits
-                            role={credit[0]}
-                            name={credit[1]}
-                            key={credit[1]}/>
-                        ))}
-                        <br />
-                        <p>Shows 2023: Hošek Contemporary, Wilde Möhre Festival, Sameheads, Gelegenheiten .</p>
-                        <p className='font-italic'></p>
-                    </>}
-                    // desde el video en youtube:
-                    // share --> embed --> copiar el link que aparece en el texto que te tira ahi en src
-                    videoYoutube=''
-                    imagenes={(
+                    description={
+                        <>
+                            Singer-songwriter presenting a work-in-progress album, Neocity.
+                            <br />
+                            An album that delves into the many sides of Berlin, a mix of hyperpop with Latin rhythms like cumbia and trap.
+                            <br />
+                            A celebration of self and yet a journey into the visceral. These openings serve as an exploration of the deeper meanings within the songs:
+                            <br />
+                            An exploration through the ritual, the sacred and the revelry, the impulsive, embracing the Latin cosmovision of finding joy despite everything.
+                        </>
+                    }
+                    credits={
+                        <>
+                            {creditos.map((credit) => (
+                                <Credits role={credit[0]} name={credit[1]} key={credit[1]} />
+                            ))}
+                            <br />
+                            <p>
+                                Shows 2023: Hošek Contemporary, Wilde Möhre Festival, Sameheads, Gelegenheiten.
+                            </p>
+                        </>
+                    }
+                    imagenes={
                         <div className="video-container">
                             {/* Vimeo Video Embed */}
                             <iframe
@@ -78,12 +67,11 @@ const Music: NextPage = () => {
                                 title="Onix Show Reel"
                             ></iframe>
                         </div>
-                    ))}
-                />            
+                    }
+                />
             </main>
         </div>
-        
-    )
-}
+    );
+};
 
-export default Music
+export default Music;
